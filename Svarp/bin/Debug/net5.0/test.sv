6ddf5a781103d4b5d9f3e3d6404b88382e989ef3
dcarlsonhornlund <dcarlsonhornlund@SE10-PF1GF6EW>
2021-10-01 20:19:38 +0200

    Add swarp code file

---
+++ Svarp/bin/Debug/net5.0/test.sv
@@ -0,0 +1,28 @@
+﻿{variable1}(Läs)"Vad heter du?"
+(Skriv)"Vad roligt att du är här:"{test}
+{variable2}"När är du född?"(Läs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+SWarp is a swedish sharp programming language that is build for fun
+
+To use:
+
+declare variables: use {}
+eg
+
+{variableName1}
+
+Usable methods:
+	(Skriv)
+	(Läs)
