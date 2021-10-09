bbe608c23020886821046f584b4256baebda4963
dcarlsonhornlund <dcarlsonhornlund@SE10-PF1GF6EW>
2021-10-09 19:13:00 +0200

    Update Grammar.txe refactore

--- Svarp/Models/CodeRow.cs
+++ Svarp/Models/CodeRow.cs
@@ -4,10 +4,8 @@ namespace Svarp
 {
     public class CodeRow
     {
-        //public List<StringVariable> StringVariables { get; set; } = new List<StringVariable>();
         public string RowVariableName { get; set; } = string.Empty;
         public string RowText { get; set; } = string.Empty;
-        //  public MethodType MethodType { get; set; }
         public string FunctionName { get; set; } = string.Empty;
         public string RowVariableValue { get; internal set; } = string.Empty;
         public string Operator { get; internal set; } = string.Empty;
