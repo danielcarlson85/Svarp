e0338b7757eee75dabb56e992aa6e627fa44de37
dcarlsonhornlund <dcarlsonhornlund@SE10-PF1GF6EW>
2021-10-09 18:37:06 +0200

    add small time test

--- Svarp/Program.cs
+++ Svarp/Program.cs
@@ -4,17 +4,45 @@ using SWarp.FileHandler;
 using SWarp.Methods;
 using SWarp.Validators;
 using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Svarp
 {
     class Program
     {
-        public static Code Code = new();
-
+        static Code Code = new();
 
         static async Task Main(string[] args)
         {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+
+
+            await Run(args);
+            //Loop();
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.Read();
+        }
+
+        static void Loop()
+        {
+            int nummer = 0;
+
+            while (nummer < 200000)
+            {
+                Console.WriteLine(nummer + "Hello world");
+                nummer++;
+            }
+        }
+
+
+        static async Task Run(string[] args)
+        {
+
             var file = await FileHandler.LoadFromFile(args);
 
             Code = CodeValidator.ValidateCode(Code, file);
@@ -23,10 +51,6 @@ namespace Svarp
             {
                 Intepreter.Run(Code, codeRow);
             }
-
-
-            Console.Read();
         }
-
     }
 }
--- Svarp/bin/Debug/net5.0/win-x64/test.sv
+++ Svarp/bin/Debug/net5.0/win-x64/test.sv
@@ -1,63 +1,55 @@
 ﻿--Detta är en kommentar
 
-(SåLänge){1<10} @(SkrivUt)"hej"@
+(SåLänge){1<200000} @(SkrivUt)"Hello World!"@
 
-
-(SåLänge){1<10}   @(SkrivUt)   "sdfsdf"    @
-
-{namn}(LäsInklTitel)"Vad heter du? "
-{ålder}(LäsInklTitel)"Hur gammal är du?"
-(SkrivUtVariabelOchText){namn}"Välkommen "
-(SkrivUtVariabelOchText){ålder}"Du är "
-
-
-
-
-{nummers}"första variabeln"
-
-(SkrivUtVariabel){nummers}
-
-
-{nummers}"uppdaterade variabeln"
-
-(SkrivUtVariabel){nummers}
-
-
-(SkrivUt)"hej"
-(RäknaUt)"4+4"
-
-
-
-
-
-(RäknaUtVariable){nummers}
-
-(SkrivUtVariabel){nummers}
-
-
-
-
-{variabel1}(LäsIn)"hej"
-
-
-
-
-
-{variabel6}"Daniel"
-(SkrivUtVariabelOchText){variabel6}"hej "
-
-(SkrivUtVariabel){nummers}
-
-(RäknaUt)"22+22"
-
-{variabel2}"Detta är värdet i variabeln"
-(SkrivUt){variabel2}
-
-(RäknaUt)"34*44"
-
-
-(LäsIn)"Vad heter du? "{var1}
-(SkrivUt)"Hej "{var1}
+--{namn}(LäsInklTitel)"Vad heter du? "
+--{ålder}(LäsInklTitel)"Hur gammal är du?"
+--(SkrivUtVariabelOchText){namn}"Välkommen "
+--(SkrivUtVariabelOchText){ålder}"Du är "
+--
+--
+--
+--
+--{nummers}"första variabeln"
+--
+--(SkrivUtVariabel){nummers}
+--
+--
+--{nummers}"uppdaterade variabeln"
+--
+--(SkrivUtVariabel){nummers}
+--
+--
+--(SkrivUt)"hej"
+--(RäknaUt)"4+4"
+--
+--
+--
+--
+--
+--(RäknaUtVariable){nummers}
+--
+--(SkrivUtVariabel){nummers}
+--
+--
+--
+--
+--{variabel1}(LäsInklTitel)"hej"
+--
+--
+--
+--
+--
+--{variabel6}"Daniel"
+--(SkrivUtVariabelOchText){variabel6}"hej "
+--(SkrivUtVariabel){nummers}
+--(RäknaUt)"22+22"
+--{variabel2}"Detta är värdet i variabeln"
+--(SkrivUtVariabel){variabel2}
+--(RäknaUt)"34*44"
+--
+--(LäsInklTitel)"Vad heter du? "{var1}
+--(SkrivUtVariabelOchText)"Hej "{var1}
 
 
 --{nummers}(LäsInklTitel)"Räkna ut: "
